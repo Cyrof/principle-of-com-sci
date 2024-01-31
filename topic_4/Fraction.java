4f9c20b9f857da99504b8eaaddda76cc8bb1d9e4
keith <keithneo00@gmail.com>
2024-01-31 15:59:50 +0800

    added topic 4

---
+++ topic_4/Fraction.java
@@ -0,0 +1,64 @@
+
+public class Fraction{
+
+    // instance variable
+    private int numerator;
+    private int denominator;
+
+    public Fraction(){
+        // fraction constructor function 
+    }
+
+    public Fraction(int numerator, int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        // setter for numerator 
+        // param numerator
+        // return None
+
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator) {
+        // setter for denominator 
+        // param denominator
+        // return None
+        if (denominator != 0){
+            this.denominator = denominator;
+        } else {
+            throw new IllegalArgumentException("Denominator cannot be 0");
+        }
+
+    }
+
+    public void display_fraction(){
+        this.switch_sign();
+        // display_fraction function to show fraction
+        System.out.printf("\n%d / %d\n", this.numerator, this.denominator);
+    }
+
+    public void switch_sign(){
+        // function to switch sign to numerator n denominator if denominator is neg
+        // param None
+        // return None
+
+        // bool notNull to check if numerator or denominator is not null
+        boolean notNull = this.numerator != 0 && this.denominator != 0 ? true : false;
+
+        // if block to check if notNull and denominator is neg than switch sign for denominator n numerator
+        if (notNull && this.denominator < 0){
+            this.denominator = - this.denominator;
+            this.numerator = - this.numerator;
+        }
+    }
+
+    public Fraction add(Fraction f2){
+        int newNumerator = (this.denominator * f2.numerator) + (f2.denominator * this.numerator);
+        int newDenominator = this.denominator * f2.denominator;
+        Fraction newFraction = new Fraction(newNumerator, newDenominator);
+        return newFraction;
+    }
+}
