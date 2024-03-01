0a8e465950e6a34661d6319435c19560f56cfb15
keith <keithneo00@gmail.com>
2024-03-01 13:48:54 +0800

    change assignment path

---
+++ assignment/codes/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Change"},{"p":"<Unnamed>","l":"Client"},{"p":"<Unnamed>","l":"CoinCountNode"},{"p":"<Unnamed>","l":"CoinDenominationCountry"},{"p":"<Unnamed>","l":"CoinNodeList"}];updateSearchResults();
