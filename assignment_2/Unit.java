791e1d78a7ae4feb362c76a2a81ee147c2b0352d
keith <keithneo00@gmail.com>
2024-03-25 21:58:39 +0800

    started assignment_2

---
+++ assignment_2/Unit.java
@@ -0,0 +1,62 @@
+/**
+ * Unit
+ */
+public class Unit {
+
+    private char enrolmentType;
+    private String grade;
+
+    public Unit(){
+        this('N');
+    }
+
+    public Unit(char enrolmentType){
+        this.enrolmentType = enrolmentType;
+        this.grade = "NA";
+    }
+
+    public void setEnrolmentType(char enrolmentType){
+        this.enrolmentType = enrolmentType;
+    }
+
+    public void setGrade(String grade){
+        this.grade = grade;
+    }
+
+    public char getEnrolmentType(){
+        return this.enrolmentType;
+    }
+
+    public String getGrade(){
+        return this.grade;
+    }
+
+    public void gradeReport(){
+        if (this.grade == "NA"){
+            System.out.println("\nGrade is not available");
+        } else {
+            System.out.printf("\nGrade: %s\n", this.grade);
+        }
+    }
+
+    public void calculateGrade(double overall_score){
+        int rounded_marks = this.round(overall_score);
+
+        if (rounded_marks >= 80){
+            this.grade = "HD";
+        } else if (rounded_marks >= 70 && rounded_marks < 80){
+            this.grade = "D";
+        } else if (rounded_marks >= 60 && rounded_marks < 70){
+            this.grade = "C";
+        } else if (rounded_marks >= 50 && rounded_marks < 60){
+            this.grade = "P";
+        } else {
+            this.grade = "N";
+        }
+    }
+
+    private int round(double num){
+        int result = (int) ((num*100)+0.5);
+        return result;
+    }
+}
---
+++ assignment_2/Unit_Course.java
@@ -0,0 +1,111 @@
+/**
+ * Unit_Course
+ */
+public class Unit_Course extends Unit{
+
+    private String unitID;
+    private int lvl;
+    private double a1_marks, a2_marks, exam_marks, overall_marks = 0;
+    private int total_marks = 300;
+
+    public Unit_Course(){
+        this('N', "None");
+    }
+
+    public Unit_Course(char type, String unitID){
+        super(type);
+        this.unitID = unitID;
+        this.setLvl();
+    }
+
+    private void setLvl(){
+        // this.lvl = Integer.parseInt(this.unitID.charAt(0));
+        this.lvl = Integer.parseInt(String.valueOf(this.unitID.charAt(3)));
+    }
+
+    public void setUnitID(String unitID){
+        this.unitID = unitID;
+        this.setLvl();
+    }
+
+    public void setA1_marks(double marks){
+        if (this.checkMarks(marks)){
+            this.a1_marks = marks;
+            this.add_Overall(marks);
+        }
+        this.calculateMarks();
+    }
+
+    public void setA2_marks(double marks){
+        if (this.checkMarks(marks)){
+            this.a2_marks = marks;
+            this.add_Overall(marks);
+        }
+      
+        this.calculateMarks();
+    }
+
+    public void setExam_marks(double marks){
+        if (this.checkMarks(marks)){
+            this.exam_marks = marks;
+            this.add_Overall(marks); 
+        }
+      
+        this.calculateMarks();
+    }
+
+    public void setOverall_mark(double marks){
+        this.overall_marks = marks;
+        this.calculateMarks();
+    }
+
+    public String getUnitID(){
+        return this.unitID;
+    }
+
+    public int getLvl(){
+        return this.lvl;
+    }
+
+    public double getA1_marks(){
+        return a1_marks;
+    }
+
+    public double getA2_marks(){
+        return a2_marks;
+    }
+
+    public double getExam_marks(){
+        return this.exam_marks;
+    }
+
+    public double getOverall_marks(){
+        return this.overall_marks;
+    }
+
+    public void add_Overall(double marks){
+        this.overall_marks += marks;
+    }
+
+    public void to_string(){
+        System.out.printf("\nUnitID: %s\nLvl: %d\nAssignment 1 marks: %.1f\nAssignment 2 marks: %.1f\nExam marks: %.1f\nOverall marks: %.1f\n", this.unitID, this.lvl, this.a1_marks, this.a2_marks, this.exam_marks, this.overall_marks);
+    }
+
+    private void calculateMarks(){
+        if (this.a1_marks!=0 && this.a2_marks!=0 && this.exam_marks!=0 && this.overall_marks!=0){
+            double marks = this.overall_marks / this.total_marks;
+            this.calculateGrade(marks);
+            System.out.println("Unit Graded.");
+        }
+
+    }
+
+    private boolean checkMarks(double marks){
+        if (marks >=0 && marks <= 100){
+            return true;
+        }
+        return false;
+    }
+
+
+}
